f25d2792aa31abfc25b6258e84e714616c4321e3
Tako-c <phakphum2phrajrern@gmail.com>
2024-03-12 22:54:32 +0700

    Fix: function serveOrder and deleteOrder

--- src/components/BaseList.vue
+++ src/components/BaseList.vue
@@ -1,23 +1,50 @@
 <script setup>
-import { defineProps } from "vue"
+import { defineProps, ref } from "vue"
+
 
 const props = defineProps({
-    orders: {
+    lists: {
+        type: Array,
+        // required: true,
+    },
+    Order_menu_list: {
         type: Array,
         required: true,
     },
 })
-console.log(props.orders)
+console.log("import :",props.Order_menu_list)
+
+
+
 </script>
 
 <template>
-    <div>
-        <slot
-            v-for="(order, index) in orders"
-            :key="index"
-            :order="order"
-        >  
-      </slot>
+    <div
+        v-for="(order_ID, index) in props.Order_menu_list"
+        :key="index"
+        class="flex"
+    >
+        <div class="p-5">
+            <p class="font-bold">Order_ID</p>
+            <slot name="Order_ID" :="order_ID"></slot>
+        </div>
+
+        <div class="flex flex-row border-2 p-5 w-screen">
+            <div
+                v-for="(order_menu, orderIndex) in order_ID.orders"
+                :key="orderIndex"
+                class="pl-5 pr-5"
+            >
+                <!-- pass data to parent component -->
+                <slot name="Order_menu" :="order_menu"></slot>
+            </div>
+        </div>
+        <!-- <button class="bg-green-600" @click="serveOrder(order_ID),deleteorder(order_ID)">
+            Serve
+        </button> -->
+    </div>
+    <div v-for="(list, index) in lists" :key="index">
+        <slot name="list" :="list"> </slot>
     </div>
 </template>
 
--- src/components/page/Listorder.vue
+++ src/components/page/Listorder.vue
@@ -1,29 +1,58 @@
-<script>
-import { ref } from "vue"
+<script setup>
 import BaseList from "../BaseList.vue"
+import orderlist from "../../../public/data/orderlist.json"
+import sales_data from "../../../public/data/sales_data.json"
+import { ref } from "vue"
 
-let test = ref([])
+const Orderlist = ref(orderlist)
+const Order_menu = ref(orderlist)
+const Sales_data = ref(sales_data)
 
-function serveOrder(order) {
-    test.value.push(order)
-    console.log(order.value)
+function deleteOrder(order_ID) {
+    const orders = order_ID.orders
+    for (const menu of orders) {
+        if (menu.selected === true) {
+            const orderIndex = orders.findIndex(order => order === menu)
+                orders.splice(orderIndex, 1)
+                console.log("total list :",Order_menu.value)
+
+        }
+    }
 }
+
+
+
+function serveOrder(order_ID) {
+    console.log("Serve button clicked:", order_ID)
+    const orders = order_ID.orders
+    for (const menu of orders) {
+        if (menu.selected === true) {
+            Sales_data.value.push(menu)
+            console.log("Selected menu:", Sales_data.value)
+        }
+    }
+}
+
 </script>
 
 <template>
-    <h1 class="text-3xl bg-green-400 w-1/6">this is page Listorder</h1>
+    <h1 class="text-3xl bg-green-400 w-1/6">This is the List Order page</h1>
 
-    <BaseList :orders="$orderlist">
-        <template #default="{ order }">
-            <div class="flex">
-                <div class="w-1/4">{{ order.menu_name }}</div>
-                <div class="w-2/4">{{ order.quantity }}</div>
-                <div class="w-auto">{{ order.details }}</div>
-                <button class="bg-green-600" @click="serveOrder(order)">
-                    Serve
-                </button>
-
-            </div>
+    <BaseList :Order_menu_list="Orderlist">
+        <template v-slot:Order_ID="order_ID">
+            <div>{{ order_ID.order_id }}</div>
+            <template v-for="(order_menu, orderIndex) in order_ID.orders" :key="orderIndex">
+                <div>{{ order_menu.menu_name }}</div>
+                <div>{{ order_menu.quantity }}</div>
+                <div>{{ order_menu.details }}</div>
+                <input type="checkbox" v-model="order_menu.selected" />
+            </template>
+            <button class="bg-green-600" @click="serveOrder(order_ID), deleteOrder(order_ID)">
+                Serve
+            </button>
         </template>
     </BaseList>
 </template>
+
+
+
