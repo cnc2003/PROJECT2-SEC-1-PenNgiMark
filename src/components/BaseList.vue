ab57881e88a2f790a301e55a1f0a5bb24eb64739
cnc2003 <chanachai.main@gmail.com>
2024-03-15 12:08:30 +0700

    [fix] : formatting code

--- src/components/BaseList.vue
+++ src/components/BaseList.vue
@@ -1,18 +1,23 @@
 <script setup>
-import {ref, watch } from "vue"
+import { ref, watch } from "vue"
 import { getOrderlist } from "../lib/fetch.js"
 
 const props = defineProps({
-    orderlist_data: Array,
+  orderlist_data: Array,
 })
 console.log(props.orderlist_data)
-
 </script>
 
 <template>
-    <div v-for="(order, index) in orderlist_data" :key="index">
-        <slot name="Order" :="{ order }"></slot>
-    </div>
+  <div
+    v-for="(order, index) in orderlist_data"
+    :key="index"
+  >
+    <slot
+      name="Order"
+      :="{ order }"
+    ></slot>
+  </div>
 </template>
 
 <style scoped></style>
