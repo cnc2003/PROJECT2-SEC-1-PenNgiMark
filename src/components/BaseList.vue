1a05c6da9f76874e3b0fb29ec1604482fdd3da87
Teamsyy <meowmeow2346@gmail.com>
2024-03-18 14:47:23 +0700

    Show menu

--- src/components/BaseList.vue
+++ src/components/BaseList.vue
@@ -4,8 +4,11 @@ import { getOrderlist } from "../lib/fetch.js"
 
 const props = defineProps({
   orderlist_data: Array,
+  menulist_data: Array,
 })
+
 console.log(props.orderlist_data)
+console.log(props.menulist_data)
 </script>
 
 <template>
@@ -16,6 +19,15 @@ console.log(props.orderlist_data)
     <slot
       name="Order"
       :="{ order }"
+    ></slot>
+  </div>
+  <div
+    v-for="(category, index) in menulist_data"
+    :key="index"
+  >
+    <slot
+      name="Category"
+      :="{ category }"
     ></slot>
   </div>
 </template>
--- src/components/page/Addmenu.vue
+++ src/components/page/Addmenu.vue
@@ -1,22 +1,63 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue"
+import { getMenulist } from "../../lib/fetch.js"
+
+const filterResult = ref(null) //default data
+
+async function fetchMenuData() {
+    filterResult.value = await getMenulist() // is array
+    for (const key in filterResult.value) {
+        console.log(key)
+    }
+}
+fetchMenuData()
+</script>
 <template>
-  <div class="flex h-full w-full">
-    <div class="border-2 border-white h-full w-3/4">
-      this must be menus list
+    <div class="flex h-full w-full">
+        <div class="border-2 border-white w-3/4">
+            this must be category list
+            <div class="flex border-2 w-1/2">
+                <div v-for="(category, key) in filterResult" :key="category"
+                class="p-3 rounded-md">
+                    <div class="bg-slate-300 p-2 rounded-md">{{ key }}</div>
+                </div>
+            </div>
+
+
+            this must be menus list
+            <div>
+              <!-- loop category -->
+              <div v-for="(category, key) in filterResult" :key="key" 
+                class="p-3 rounded-md">
+
+                <!-- loop menulist in category  -->
+                <div v-for="menus in category"
+                class="border-2 p-2 m-1">
+                
+                    <!-- loop menu in menulist  -->
+                    <div v-for="menu in menus">
+                      <div>{{ menu }}</div>
+                    </div>
+                </div>
+                    
+                </div>
+            </div>
+        </div>
+        <div class="flex flex-col border-2 border-black h-dvh w-1/4">
+            <div class="border-2 border-white m-3 h-16">
+                <button class="border-2 border-white">Dine in</button>
+                <button class="border-2 border-white">Take away</button>
+            </div>
+            <div class="border-2 border-white m-3 h-full">Total Items</div>
+            <div class="border-2 border-white m-3 h-1/5">Payment Summary</div>
+            <div class="border-2 border-white m-3 h-28">
+                <button class="border-2 border-white">Cash</button>
+                <button class="border-2 border-white">Card</button>
+                <button class="border-2 border-white">QR</button>
+            </div>
+            <button class="border-2 border-white h-20 m-3 mb-6">
+                Place Order
+            </button>
+        </div>
     </div>
-    <div class="flex flex-col border-2 border-black h-dvh w-1/4">
-      <div class="border-2 border-white m-3 h-16">
-        <button class="border-2 border-white">Dine in</button>
-        <button class="border-2 border-white">Take away</button>
-      </div>
-      <div class="border-2 border-white m-3 h-full">Total Items</div>
-      <div class="border-2 border-white m-3 h-1/5">Payment Summary</div>
-      <div class="border-2 border-white m-3 h-28">
-        <button class="border-2 border-white">Cash</button>
-        <button class="border-2 border-white">Card</button>
-        <button class="border-2 border-white">QR</button>
-      </div>
-      <button class="border-2 border-white h-20 m-3 mb-6">Place Order</button>
-    </div>
-  </div>
 </template>
