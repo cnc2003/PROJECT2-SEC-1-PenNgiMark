0a7b997fc4c7f078e00f14f1597d65fcea9ce106
cnc2003 <chanachai.main@gmail.com>
2024-03-30 00:27:24 +0700

    [add] icon in navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,38 +1,40 @@
 <script setup>
 import { ref } from "vue"
 
-const li = ref("rounded-md w-full p-4 text-gray-500 font-semibold")
-const action = ref("bg-gray-200 text-gray-800 ")
+const li = ref("rounded-md w-full p-4 text-gray-500  text-lg font-semibold flex flex-row items-center gap-2")
+const action = ref("bg-gray-200 text-gray-800 scale-[103%] shadow-md")
+const svg = ref("size-8")
 </script>
 
 <template>
   <div class="flex flex-col p-4 gap-2 w-full h-screen bg-base-200 text-base-content">
     <!-- Sidebar content here -->
 
-    <router-link
-      to="/"
-      class="hover:bg-gray-200 hover:text-gray-800"
-      :class="li"
-      :active-class="action"
-      :hover="action"
-    >
-      Addmenu
+    <router-link to="/" class="hover:bg-gray-200 hover:text-gray-800 hover:shadow-sm" :class="li" :active-class="action"
+      :hover="action">
+      <svg dataSlot="icon" fill="none" strokeWidth="{1.5}" stroke="currentColor" viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg" aria-hidden="true" :class="svg">
+        <path strokeLinecap="round" strokeLinejoin="round"
+          d="M13.5 16.875h3.375m0 0h3.375m-3.375 0V13.5m0 3.375v3.375M6 10.5h2.25a2.25 2.25 0 0 0 2.25-2.25V6a2.25 2.25 0 0 0-2.25-2.25H6A2.25 2.25 0 0 0 3.75 6v2.25A2.25 2.25 0 0 0 6 10.5Zm0 9.75h2.25A2.25 2.25 0 0 0 10.5 18v-2.25a2.25 2.25 0 0 0-2.25-2.25H6a2.25 2.25 0 0 0-2.25 2.25V18A2.25 2.25 0 0 0 6 20.25Zm9.75-9.75H18a2.25 2.25 0 0 0 2.25-2.25V6A2.25 2.25 0 0 0 18 3.75h-2.25A2.25 2.25 0 0 0 13.5 6v2.25a2.25 2.25 0 0 0 2.25 2.25Z" />
+      </svg>
+
+      <h1>Order menu</h1>
     </router-link>
-    <router-link
-      to="/list-order"
-      class="hover:bg-gray-200 hover:text-gray-800"
-      :class="li"
-      :active-class="action"
-    >
-      Listorder
+    <router-link to="/list-order" class="hover:bg-gray-200 hover:text-gray-800 hover:shadow-sm" :class="li" :active-class="action">
+      <svg dataSlot="icon" fill="none" strokeWidth="{1.5}" stroke="currentColor" viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg" aria-hidden="true" :class="svg">
+        <path strokeLinecap="round" strokeLinejoin="round"
+          d="M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 0 0 2.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 0 0-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 0 0 .75-.75 2.25 2.25 0 0 0-.1-.664m-5.8 0A2.251 2.251 0 0 1 13.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25ZM6.75 12h.008v.008H6.75V12Zm0 3h.008v.008H6.75V15Zm0 3h.008v.008H6.75V18Z" />
+      </svg>
+      <h1>Listorder</h1>
     </router-link>
-    <router-link
-      to="/manegement"
-      class="hover:bg-gray-200 hover:text-gray-800"
-      :class="li"
-      :active-class="action"
-    >
-      Management
+    <router-link to="/manegement" class="hover:bg-gray-200 hover:text-gray-800 hover:shadow-sm" :class="li" :active-class="action">
+      <svg dataSlot="icon" fill="none" strokeWidth="{1.5}" stroke="currentColor" viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg" aria-hidden="true" :class="svg">
+        <path strokeLinecap="round" strokeLinejoin="round"
+          d="M7.5 14.25v2.25m3-4.5v4.5m3-6.75v6.75m3-9v9M6 20.25h12A2.25 2.25 0 0 0 20.25 18V6A2.25 2.25 0 0 0 18 3.75H6A2.25 2.25 0 0 0 3.75 6v12A2.25 2.25 0 0 0 6 20.25Z" />
+      </svg>
+      <h1>Management</h1>
     </router-link>
   </div>
 </template>
