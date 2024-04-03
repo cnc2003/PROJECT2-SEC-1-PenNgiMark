146409a1d832ec127b8e5768088e0842a53fe26f
pannawatfriend <friendlysripa@gmail.com>
2024-04-03 23:49:29 +0700

    chore: format document

--- src/components/Analysis.vue
+++ src/components/Analysis.vue
@@ -1,3 +1,53 @@
+<script setup>
+import { getList } from "../lib/fetch"
+import { ref } from "vue"
+import JsxIconBase from "./JsxIconBase.vue"
+
+const partBox = ref("w-1/3 h-24  flex flex-row gap-4 p-4 items-center")
+const header = ref("font-medium text-gray-500 text-lg")
+const number = ref("font-bold text-3xl")
+
+const totalOrder = ref(0)
+const totalSold = ref(0)
+const orderData = ref(null)
+const managementData = ref(null)
+const props = defineProps({
+    totalMenu: Number,
+    totalSold: Number,
+    hr: String,
+})
+
+async function fetchOrderData() {
+    orderData.value = await getList("Management")
+    totalOrder.value = orderData.value.length
+    calTotalOrder()
+}
+
+function calTotalOrder() {
+    const orderQuantities = managementData.value.map((order) => {
+        const totalQuantity = order.menus.reduce(
+            (acc, curr) => acc + curr.quantity,
+            0
+        )
+        return totalQuantity
+    })
+    const totalQuantities = [...orderQuantities]
+    totalSold.value = totalQuantities.reduce((acc, curr) => acc + curr, 0)
+    //console.log(totalSold.value)
+}
+async function fetchMenagementData() {
+    managementData.value = await getList("Management")
+    for (const cate in managementData.value) {
+        // cate [ex index = 0,1,2,3,4,5]
+        const category = managementData.value[cate]
+        totalSold.value += category.menus.length
+        // console.log(totalSold.value);
+    }
+}
+fetchMenagementData()
+fetchOrderData()
+</script>
+
 <template>
     <div
         name="analysis"
@@ -47,55 +97,3 @@
         </div>
     </div>
 </template>
-
-<script setup>
-import { getList } from "../lib/fetch"
-import { ref } from "vue"
-import JsxIconBase from "./JsxIconBase.vue";
-
-const partBox = ref("w-1/3 h-24  flex flex-row gap-4 p-4 items-center")
-const header = ref("font-medium text-gray-500 text-lg")
-const number = ref("font-bold text-3xl")
-
-const totalOrder = ref(0)
-const totalSold = ref(0)
-const orderData = ref(null)
-const managementData = ref(null)
-const props = defineProps({
-    totalMenu: Number,
-    totalSold: Number,
-    hr: String,
-})
-
-async function fetchOrderData() {
-    orderData.value = await getList("Management")
-    totalOrder.value = orderData.value.length
-    calTotalOrder()
-}
-
-function calTotalOrder() {
-    const orderQuantities = managementData.value.map((order) => {
-        const totalQuantity = order.menus.reduce(
-            (acc, curr) => acc + curr.quantity,
-            0
-        )
-        return totalQuantity
-    })
-    const totalQuantities = [...orderQuantities]
-    totalSold.value = totalQuantities.reduce((acc, curr) => acc + curr, 0)
-    //console.log(totalSold.value)
-}
-async function fetchMenagementData() {
-    managementData.value = await getList("Management")
-    for (const cate in managementData.value) {
-        // cate [ex index = 0,1,2,3,4,5]
-        const category = managementData.value[cate]
-        totalSold.value += category.menus.length
-        // console.log(totalSold.value);
-    }
-}
-fetchMenagementData()
-fetchOrderData()
-</script>
-
-<style></style>
