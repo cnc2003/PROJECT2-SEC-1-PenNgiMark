cd4505e649b9ad045bd62ea80d7e23974e7425f2
cnc2003 <chanachai.main@gmail.com>
2024-04-03 16:11:17 +0700

    [dec] cart

--- src/components/page/Addmenu.vue
+++ src/components/page/Addmenu.vue
@@ -1,52 +1,52 @@
 <script setup>
-import { ref, watch, computed, onMounted } from "vue";
+import { ref, watch, computed, onMounted } from "vue"
 
-import { getList, PostMenu } from "../../lib/fetch.js";
-import CartList from "../CartList.vue";
-import JsxIconBase from "../JsxIconBase.vue";
-import MenuBaseCard from "../MenuBaseCard.vue";
-import ModalConfirm from "../ModalConfirm.vue";
+import { getList, PostMenu } from "../../lib/fetch.js"
+import CartList from "../CartList.vue"
+import JsxIconBase from "../JsxIconBase.vue"
+import MenuBaseCard from "../MenuBaseCard.vue"
+import ModalConfirm from "../ModalConfirm.vue"
 
-const filterResult = ref(null); //default data
-const afterFilterResult = ref(null); // default value
-const promotions = ref([]);
-const discount = ref(0);
-const subtotalPrice = ref(0);
-const menusInCart = ref([]);
-const paymentMethod = ref("");
-let selectedmenus = [];
-const showModalConfirm = ref(false);
-const modalAction = ref("");
-const allSelected = ref(true);
+const filterResult = ref(null) //default data
+const afterFilterResult = ref(null) // default value
+const promotions = ref([])
+const discount = ref(0)
+const subtotalPrice = ref(0)
+const menusInCart = ref([])
+const paymentMethod = ref("")
+let selectedmenus = []
+const showModalConfirm = ref(false)
+const modalAction = ref("")
+const allSelected = ref(true)
 
 async function fetchMenuData() {
-  filterResult.value = await getList("Menus"); // is array
+  filterResult.value = await getList("Menus") // is array
   // console.log(filterResult.value)
 }
-fetchMenuData();
+fetchMenuData()
 
 onMounted(async () => {
   const [menusRes, promotionsRes] = await Promise.all([
     getList("Menus"),
     getList("Promotions"),
-  ]);
-  fetchMenuData();
-  promotions.value = promotionsRes;
+  ])
+  fetchMenuData()
+  promotions.value = promotionsRes
   // filterResult.value = menusRes
-});
+})
 
 function filterCategory(inputCategory) {
   // console.log(inputCategory)
   if (inputCategory === null || inputCategory === "All") {
-    afterFilterResult.value = filterResult.value;
+    afterFilterResult.value = filterResult.value
   } else {
     for (const data in filterResult.value) {
-      const categorykey = filterResult.value[data]; // category in menu
+      const categorykey = filterResult.value[data] // category in menu
       // // console.log(categorykey.category)
 
       if (inputCategory === categorykey.category) {
         // console.log(categorykey)
-        afterFilterResult.value = [categorykey];
+        afterFilterResult.value = [categorykey]
       }
     }
   }
@@ -81,62 +81,62 @@ const mocDrinks = [
 // const menusInCart = ref(mocDrinks)
 
 const calculateDiscount = () => {
-  let totalDiscount = 0;
+  let totalDiscount = 0
 
   for (const promotion of promotions.value) {
     if (
       promotion.menus.every((promoItem) => {
         const cartItem = menusInCart.value.find(
           (item) => item.menu_name === promoItem.menuName
-        );
-        return cartItem && cartItem.quantity >= promoItem.quantity;
+        )
+        return cartItem && cartItem.quantity >= promoItem.quantity
       })
     ) {
-      const discountAmount = promotion.discount;
-      totalDiscount += discountAmount;
+      const discountAmount = promotion.discount
+      totalDiscount += discountAmount
     }
   }
-  discount.value = totalDiscount;
+  discount.value = totalDiscount
   // return totalDiscount
-};
+}
 
 const getSubtotalPrice = () => {
-  let price = 0;
+  let price = 0
   menusInCart.value.forEach((item) => {
-    price += item.price * item.quantity;
-  });
-  subtotalPrice.value = price;
-};
+    price += item.price * item.quantity
+  })
+  subtotalPrice.value = price
+}
 watch(
   () => menusInCart,
   (newCart) => {
     // // console.log(newCart.value)
-    calculateDiscount();
-    getSubtotalPrice();
+    calculateDiscount()
+    getSubtotalPrice()
   },
   { deep: true, immediate: true }
-);
+)
 
 const totalPrice = computed(() => {
-  return subtotalPrice.value - discount.value;
-});
+  return subtotalPrice.value - discount.value
+})
 
 const openModal = (action) => {
   if (!showModalConfirm.value) {
-    showModalConfirm.value = true;
+    showModalConfirm.value = true
   }
-  modalAction.value = action;
-};
+  modalAction.value = action
+}
 
 const placeOrder = async () => {
   // console.log("Place Order")
   if (menusInCart.value.length === 0) {
-    openModal("EmptyCart");
-    return;
+    openModal("EmptyCart")
+    return
   }
   if (paymentMethod.value === "") {
-    openModal("EmptyPayment");
-    return;
+    openModal("EmptyPayment")
+    return
   }
   const addOrderRes_toListOrder = await PostMenu(
     {
@@ -155,7 +155,7 @@ const placeOrder = async () => {
       totalPrice: totalPrice.value,
     },
     "Management"
-  );
+  )
   // console.log({ addOrderRes })
 
   // if (addOrderRes !== 201) {
@@ -163,28 +163,28 @@ const placeOrder = async () => {
   //   return;
   // }
   // orderNumber.value++
-  menusInCart.value = [];
-  paymentMethod.value = "";
-};
+  menusInCart.value = []
+  paymentMethod.value = ""
+}
 
 function ToggleClick(item) {
   // console.log(selectedmenus)
   if (selectedmenus.length > 0) {
-    selectedmenus[0].selected = false;
-    selectedmenus.shift();
+    selectedmenus[0].selected = false
+    selectedmenus.shift()
   }
-  item.selected = true;
-  selectedmenus.push(item);
+  item.selected = true
+  selectedmenus.push(item)
 }
 
 function confirmOption(item, propoty) {
   // console.log(propoty.value)
   if (item.sweetnessLevel === undefined || item.sweetnessLevel === "") {
     // alert("Please select sweetness level")
-    openModal("SweetnessLevel");
-    return;
+    openModal("SweetnessLevel")
+    return
   }
-  selectedmenus[0].selected = false;
+  selectedmenus[0].selected = false
 
   let addToCart = {
     menu_name: item.menu_name,
@@ -193,46 +193,55 @@ function confirmOption(item, propoty) {
     selected: item.selected,
     sweetnessLevel: item.sweetnessLevel,
     category: propoty.category,
-  };
-  fetchMenuData();
-  menusInCart.value.push(addToCart);
+  }
+  fetchMenuData()
+  menusInCart.value.push(addToCart)
 }
 function cancelOption(item) {
-  selectedmenus[0].selected = false;
-  fetchMenuData();
+  selectedmenus[0].selected = false
+  fetchMenuData()
 }
 
 function selectedCategory(category) {
   for (const data in filterResult.value) {
-    const categorykey = filterResult.value[data];
+    const categorykey = filterResult.value[data]
     if (category === "All" || category === categorykey.category) {
-      categorykey.selected = true;
-      allSelected === false;
+      categorykey.selected = true
+      allSelected === false
     } else {
-      categorykey.selected = false;
-      allSelected === true;
+      categorykey.selected = false
+      allSelected === true
     }
   }
 }
 
 function closeOtherButtons() {
   for (const data in filterResult.value) {
-    const categorykey = filterResult.value[data];
-    categorykey.selected = false;
+    const categorykey = filterResult.value[data]
+    categorykey.selected = false
   }
 }
 
 const sweetBtn = ref(
   "py-2 w-[25%] text-black rounded-2xl border border-1 cursor-pointer text-sm text-center font-semibold hover:scale-105 transition duration-300 hover:bg-pink-100 hover:border-pink-500"
-);
+)
 </script>
 <template>
   <div class="flex h-full w-full shrink-0">
-    <section class="border-2 border-white w-3/4 h-full p-4" @click="closeModal">
+    <section
+      class="border-2 border-white w-3/4 h-full p-4"
+      @click="closeModal"
+    >
       <!-- this must be category list -->
-      <div name="category" class="flex flex-col w-full p-4">
+      <div
+        name="category"
+        class="flex flex-col w-full p-4"
+      >
         <h1 class="text-3xl font-bold mb-4">Categories</h1>
-        <div name="categoryContainer" class="flex flex-row flex-wrap">
+        <div
+          name="categoryContainer"
+          class="flex flex-row flex-wrap"
+        >
           <div
             :class="{
               'bg-blue-700 text-white': allSelected,
@@ -241,10 +250,10 @@ const sweetBtn = ref(
             class="p-6 px-8 m-2 rounded-xl btn btn-md font-semibold text-lg flex content-center"
             @click="
               () => {
-                filterCategory('All');
-                selectedCategory('All');
-                allSelected = true;
-                closeOtherButtons();
+                filterCategory('All')
+                selectedCategory('All')
+                allSelected = true
+                closeOtherButtons()
               }
             "
           >
@@ -256,9 +265,9 @@ const sweetBtn = ref(
             class="p-4 px-6 m-2 rounded-xl btn btn-md font-semibold text-lg flex content-center"
             @click="
               () => {
-                filterCategory(propoty.category);
-                selectedCategory(propoty.category);
-                allSelected = false;
+                filterCategory(propoty.category)
+                selectedCategory(propoty.category)
+                allSelected = false
               }
             "
             :style="
@@ -295,11 +304,13 @@ const sweetBtn = ref(
           <div
             v-for="(item, key) in propoty.menus"
             :key="key"
-            name="menuContainer"
           >
             <div @click="ToggleClick(item)">
               <MenuBaseCard>
-                <template #title v-if="!item.selected">
+                <template
+                  #title
+                  v-if="!item.selected"
+                >
                   <figure class="image-full min-h-44 max-h-32 w-70">
                     <img
                       :src="item.img_src"
@@ -324,16 +335,27 @@ const sweetBtn = ref(
                   </div>
                 </template>
 
-                <template #modal v-if="item.selected">
+                <template
+                  #modal
+                  v-if="item.selected"
+                >
                   <div
                     class="flex flex-col items-center justify-center card w-80 h-96 bg-[url(item.img_src)]"
                   >
                     <!-- <img :src="item.img_src" alt="img" class="absolute opacity-80"> -->
 
-                    <img src="/src/assets/icon/sugar-pink.png" alt="sugar" class="size-20">
-                   <div class="flex justify-center w-full ">
-                    <p class="text-2xl font-bold text-pink-500 my-4 divider divider-secondary w-[90%]">Sugar Level</p>
-                  </div>
+                    <img
+                      src="/src/assets/icon/sugar-pink.png"
+                      alt="sugar"
+                      class="size-20"
+                    />
+                    <div class="flex justify-center w-full">
+                      <p
+                        class="text-2xl font-bold text-pink-500 my-4 divider divider-secondary w-[90%]"
+                      >
+                        Sugar Level
+                      </p>
+                    </div>
                     <div
                       class="text-xl w-full border-opacity-50 flex flex-row gap-2 justify-center my-4"
                     >
@@ -341,21 +363,21 @@ const sweetBtn = ref(
                         type="radio"
                         id="no_sugar"
                         name="sweetLevel"
-                        value="no_sugar"
+                        value="No sugar"
                         v-model="item.sweetnessLevel"
                         class="hidden peer/no_sugar"
                       /><label
                         for="no_sugar"
-                        class="peer-checked/no_sugar:bg-pink-200 hover:text-pink-400 "
+                        class="peer-checked/no_sugar:bg-pink-200 hover:text-pink-400"
                         :class="sweetBtn"
-                        >
+                      >
                         No Sugar</label
                       >
                       <input
                         type="radio"
                         id="low_sugar"
                         name="sweetLevel"
-                        value="low_sugar"
+                        value="Low sugar"
                         v-model="item.sweetnessLevel"
                         class="hidden peer/low_sugar"
                       />
@@ -382,7 +404,9 @@ const sweetBtn = ref(
                       >
                     </div>
                     <br />
-                    <div class="flex w-full h-auto justify-center items-end gap-2">
+                    <div
+                      class="flex w-full h-auto justify-center items-end gap-2"
+                    >
                       <button
                         class="hover:bg-yellow-400 cursor-pointer bg-yellow-100 text-slate-900 font-semibold py-2 px-8 border border-yellow-500 rounded-xl shadow hover:underline hover:transition hover:duration-300 underline-offset-4"
                         @click.stop="cancelOption(item)"
@@ -407,59 +431,75 @@ const sweetBtn = ref(
     <!-----------------------------------
      ---------- Cart section ------------
      ------------------------------------>
-    <section class="flex flex-col gap-2 bg-base-200 h-full w-1/4 p-4 justify-center">
-      <div class="join join-horizontal rounded-2xl bg-white h-20 w-full justify-center py-2">
-        <button class="join-item btn rounded-sm h-full w-[48%]">Dine in</button>
-        <button class="join-item btn rounded-sm h-full w-[48%]">Take away</button>
+    <section
+      class="flex flex-col gap-2 bg-base-200 h-full w-1/4 p-4 justify-center"
+    >
+      <div
+        class="flex items-center bg-white h-[8%] w-full justify-center py-2 px-2 rounded-2xl"
+      >
+        <button
+          class="grid h-14 flex-grow card  bg-orange-500 rounded-xl place-items-center w-auto font-semibold text-xl text-white hover:scale-95 hover:bg-orange-300 hover:underline underline-offset-4 transition duration-300 ease-linear"
+        >
+          Dine in
+        </button>
+        <div class="divider divider-horizontal mx-2"></div>
+        <button
+          class="grid h-14 flex-grow card bg-yellow-400 rounded-box place-items-center w-auto font-semibold text-xl text-white hover:scale-95 hover:bg-yellow-300 hover:underline underline-offset-4 transition duration-300 ease-linear"
+        >
+          Take away
+        </button>
       </div>
-      <CartList :menusInCart="menusInCart" />
-      <div class="border-2 border-black m-2 h-1/5">
-        Payment Summary
+      <CartList
+        :menusInCart="menusInCart"
+        class="h-[60%]"
+      />
+      <div class="bg-white h-[16%] w-full py-2 px-2 rounded-2xl text-lg">
+        <span class="font-semibold">Payment Summary</span>
+        <span class="divider my-[1px]"></span>
         <div class="flex justify-between">
           <p>Subtotal</p>
-          <p>{{ subtotalPrice }}</p>
+          <p>{{ subtotalPrice }} ฿</p>
         </div>
         <div class="flex justify-between">
           <p>Discount</p>
-          <p>
-            {{ discount }}
-          </p>
+          <p>{{ discount }} ฿</p>
         </div>
-        <hr class="mx-3" />
+        <span class="divider my-[1px]"></span>
         <div class="flex justify-between">
           <p>Total Price</p>
-          <p>
-            {{ totalPrice }}
-          </p>
+          <p>{{ totalPrice }} ฿</p>
         </div>
       </div>
       <div
-        class="flex flex-col justify-evenly border-2 border-black h-28 mx-2 px-2 pb-2"
+        class="flex flex-col justify-evenly h-[10%] p-2 bg-white w-full rounded-2xl text-lg "
       >
-        Payment Method
+        <span class="font-semibold pb-2"> Payment Method </span>
         <div class="flex justify-evenly h-12">
           <button
-            class="flex justify-center items-center border-2 border-black rounded-md h-full w-16"
+            class="flex justify-center gap-2 items-center rounded-md h-full w-[28%] px-4 hover:bg-green-400 transition duration-300 ease-linear"
             @click="paymentMethod = 'cash'"
           >
             <JsxIconBase iconName="Cash" />Cash
           </button>
+          <div class="divider divider-horizontal mx-[2px]"></div>
           <button
-            class="flex justify-center items-center border-2 border-black rounded-md h-full w-16"
+            class="flex justify-center gap-2 items-center rounded-md h-full w-[28%] px-4 hover:bg-yellow-400 transition duration-300 ease-linear"
             @click="paymentMethod = 'card'"
           >
             <JsxIconBase iconName="Card" />Card
           </button>
+          <div class="divider divider-horizontal mx-[2px]"></div>
           <button
-            class="flex justify-center items-center border-2 border-black rounded-md h-full w-16"
+            class="flex justify-center gap-2 items-center rounded-md h-full w-[28%] px-4 hover:bg-sky-400 transition duration-300 ease-linear"
             @click="paymentMethod = 'QR'"
           >
             <JsxIconBase iconName="QR" />QR
           </button>
         </div>
       </div>
+
       <button
-        class="border-2 border-black h-20 m-2 mb-6"
+        class="h-[5%] w-full bg-amber-500 rounded-2xl font-semibold text-xl hover:bg-orange-600 hover:scale-95 transition duration-300 ease-linear"
         @click="openModal('placeOrder')"
       >
         Place Order
@@ -479,5 +519,11 @@ const sweetBtn = ref(
 .selected {
   background-color: blue;
   color: white;
+}
+
+.nuero {
+  border-radius: 1rem;
+  background: #ffffff;
+  box-shadow: inset 6px 6px 11px #ebebeb, inset -6px -6px 11px #ffffff;
 }
 </style>
